8454bfcc5e9c93af1fcd4508c4a30aef33a510ce
simao234430 <simao234430@gmail.com>
2019-03-07 18:44:38 +0800

    logic error

--- django_web/views/index.py
+++ django_web/views/index.py
@@ -1,8 +1,9 @@
+# -*- coding: utf-8 -*-
 from __future__ import unicode_literals
 from django.http import HttpResponse
 from django.shortcuts import render, render_to_response
 import datetime
-from ..models import Article
+from ..models import Article,TeacherInfo,TeacherCategory
 # Create your views here.
 def index(request):
     context = {}
@@ -10,7 +11,11 @@ def index(request):
     # for i in list:
     #     print i
     Article_list = Article.objects.all()[:3]
-    return render(request, 'index.html', {'list': Article_list})
+    Leader_list = TeacherCategory.objects.get(category='领导').teacher.all().order_by('-range_weight')[:4]
+
+    for i in Leader_list:
+        print i
+    return render(request, 'index.html', {'list': Article_list,'Leader_list': Leader_list})
 
 def test(request):
     return render_to_response('enroll.html')
